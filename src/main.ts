fd93e4c40fac21f2aa76bdf4b4711b6618cc5219
mgporter <26415062+mgporter@users.noreply.github.com>
2025-01-29 16:35:53 -0500

    getname

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,17 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const name = core.getInput("firstname");
+    core.info(`Person's name is ${name}`)
+    
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.exportVariable('personname', name);
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+
+
+    
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
